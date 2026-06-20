367b5bdb127e08ff9defb70c41d45bc50ed09c4d
louisyanne <louisianekameni@gmail.com>
2026-06-19 22:43:13 -0400

    CI/CD + change method

--- src/orders/commands/write_order.py
+++ src/orders/commands/write_order.py
@@ -112,14 +112,17 @@ def request_payment_link(order_id, total_amount, user_id):
         "total_amount": total_amount
     }
 
-    # TODO: Requête à POST /payments
-    print("")
-    response_from_payment_service = {}
+    response_from_payment_service = requests.post(
+        'http://api-gateway:8080/payments-api/payments',
+        json=payment_transaction,
+        headers={'Content-Type': 'application/json'}
+    )
 
-    if True: # if response.ok
+    if response_from_payment_service.status_code == 201:
+        payment_id = response_from_payment_service.json()["payment_id"]
         print(f"ID paiement: {payment_id}")
 
-    return f"http://api-gateway:8080/payments-api/payments/process/{payment_id}" 
+    return f"http://api-gateway:8080/payments-api/payments/process/{payment_id}"
 
 def delete_order(order_id: int):
     """Delete order in MySQL, keep Redis in sync"""
